a323f189f27f674c2dbc11229ab8ba3b9caba46b
plooody <plooody@gmail.com>
2022-07-06 08:39:52 +0200

    changed structure to be less messy

--- contentsmith/crm/apps.py
+++ contentsmith/crm/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CrmsmithConfig(AppConfig):
+class CrmConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'crmsmith'
+    name = 'crm'
